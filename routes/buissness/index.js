b77004a601ca40066bb1acf016861cf123a5d70c
IshanSingla <is.ishan.singla@gmail.com>
2023-01-21 10:29:55 +0530

    Update: Bugs In buissness api

--- routes/buissness/index.js
+++ routes/buissness/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const buissnessExpense = require("../../schema/buissness/buissnessExpense");
-const userProfile = require("../../schema/user/userProfile")
+const userProfile = require("../../schema/user/userProfile");
 
 router.get("/", async (req, res) => {
   const { mongodbUser } = req.user;
@@ -31,30 +31,30 @@ router.use(
 router.post("/create", async (req, res) => {
   const { mongodbUser } = req.user;
   const { buissnessName, buissnessGstNo } = req.body;
-  let da = new buissnessExpense(
-    {
-      buissnessName,
-      buissnessGstNo,
-      buissnessExpensesTypes: [],
-      buissnessCustomers: [],
-      createdBy: mongodbUser._id,
-    }, 
-  );
-  let data=await da.save();
-  await userProfile.findByIdAndUpdate(
-    mongodbUser._id,
-    { $push: { buissnessExpense: data._id } },
-    { new: true },
-    (err, doc) => {
-      if (err) {
-        res.status(500).json({ message: "Error creating buissness" });
-      } else {
-        res
-          .status(200)
-          .json({ message: "Buissness created successfully" });
-      }
-    }
-  );
+  let da = new buissnessExpense({
+    buissnessName,
+    buissnessGstNo,
+    buissnessExpensesTypes: [],
+    buissnessCustomers: [],
+    createdBy: mongodbUser._id,
+  });
+  let data = await da.save();
+  res.status(200).json({ message: "Buissness created successfully" });
+
+  // await userProfile.findByIdAndUpdate(
+  //   mongodbUser._id,
+  //   { $push: { buissnessExpense: data._id } },
+  //   { new: true },
+  //   (err, doc) => {
+  //     if (err) {
+  //       res.status(500).json({ message: "Error creating buissness" });
+  //     } else {
+  //       res
+  //         .status(200)
+  //         .json({ message: "Buissness created successfully" });
+  //     }
+  //   }
+  // );
 });
 
 module.exports = router;
