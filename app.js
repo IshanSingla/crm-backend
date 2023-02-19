b2e3599d9659f38f610086d9a5326da4b7887d7e
IshanSingla <is.ishan.singla@gmail.com>
2023-02-19 12:40:34 +0530

    Update: bug fixed in  Cors

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv");
-import cors from "cors";
+const cors = require("cors");
 const { errorHandler } = require("./middleware/errorHandler");
 // const { morganImpl } = require("./configs/morgan");
 const { connectDatabase } = require("./configs/mongoose");
 const { getIPAddress } = require("./utils/getIPAddress");
+
 
 const app = express();
 dotenv.config();
 
 connectDatabase();
 
-app
-  .use(express.json())
-  ,use(cors())
-  // .use(firebaseAuth)
-  // .use(morganImpl)
-  .use("/", require("./routes"))
-  .use(errorHandler);
+app.use(express.json()),
+  use(cors())
+    // .use(firebaseAuth)
+    // .use(morganImpl)
+    .use("/", require("./routes"))
+    .use(errorHandler);
 
 const PORT = Number(process.env.PORT) || 4000;
 
