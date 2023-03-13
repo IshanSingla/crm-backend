b990d23b16420aaba7cf3b1ad136dfcb943ff2b6
IshanSingla <is.ishan.singla@gmail.com>
2023-03-13 22:14:54 +0530

    Update: Ip Checker

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,27 @@
+const { request } = require("express");
 const { firebaseAuth } = require("../middleware/firebaseAuth");
+const axios = require("axios");
 
 const router = require("express").Router();
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to the CRM" });
+// router.get("/", (req, res) => {
+//   res.status(200).json({ message: "Welcome to the CRM" });
+// });
+router.get("/", function (req, res) {
+  const ipAddress = req.socket.remoteAddress.replace("::ffff:", "");
+  res.json({ hearder: req.headers["user-agent"], ipAddress });
+  // axios
+  //   .get("https://www.iplocate.io/api/lookup/" + ipAddress, {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Accept: "application/json",
+  //       // Authorization: "Bearer " + process.env.IPLOCATE_API_KEY,
+  //     },
+  //   })
+  //   .then((response) => {
+  //     console.log(req.headers);
+  //     res.json({ ...response.data });
+  //   });
+  // res.json({ ip: ipAddress });
 });
 
 router.use("/api/v1", firebaseAuth, require("./v1"));
